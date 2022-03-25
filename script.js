0974b35f038a7abaea2c1c6c2308b211255bdfa0
apodacaj1995 <contactjackiecodes@gmail.com>
2022-03-24 19:29:21 -0600

    updated index.js
    appended data to the DOM

--- script.js
+++ script.js
@@ -18,31 +18,70 @@ async function getMovies(url){
     showMovies(data.results)
 }
 
-function showMovies(movies){
-    //to clear main
+function showMovies (movies){
+    //to clear main screen after search
     main.innerHTML = ''
 
     movies.forEach((movie) => {
-        const { title, poster_path, vote_average, overview } = movie
+        const { title, poster_path, vote_average, overview} = movie
 
-        const movieEL = document.createElement('div')
-        movieEL.classList.add('movie')
+        const movieEl = document.createElement('div')
+        movieEl.classList.add('movie')
 
-        movieEl.innerHTML = `            
+        movieEl.innerHTML = `
             <img src="${IMG_PATH + poster_path}" alt="${title}">
-            <div class="movie-info">
+             <div class="movie-info">
                 <h3>${title}</h3>
                 <span class="${getClassByRate(vote_average)}">${vote_average}</span>
-            </div>
-            <div class="overview">
-               <h3>Overview</h3>
-                ${overview}
-            </div>
+                </div>
+                <div class="overview">
+                    <h3>Overview</h3>
+                        ${overview}
+                </div>
         `
-// TO PUT INTO THE DOM
-        main.appendChild(movieEL)
+        main.appendChild(movieEl)
     })
 }
+
+function getClassByRate(vote){
+    if(vote >= 8){
+        return 'green'
+    }else if(vote >= 5){
+        return 'orange'
+    }else {
+        return 'red'
+    }
+}
+
+
+
+
+
+// function showMovies(movies){
+//     //to clear main
+//     main.innerHTML = ''
+
+//     movies.forEach((movie) => {
+//         const { title, poster_path, vote_average, overview } = movie
+
+//         const movieEL = document.createElement('div')
+//         movieEL.classList.add('movie')
+
+//         movieEl.innerHTML = `            
+//             <img src="${IMG_PATH + poster_path}" alt="${title}">
+//             <div class="movie-info">
+//                 <h3>${title}</h3>
+//                 <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+//             </div>
+//             <div class="overview">
+//                <h3>Overview</h3>
+//                 ${overview}
+//             </div>
+//         `
+// // TO PUT INTO THE DOM
+//         main.appendChild(movieEL)
+//     })
+// }
 
 //get class by ranking
 function getClassByRate(vote){
